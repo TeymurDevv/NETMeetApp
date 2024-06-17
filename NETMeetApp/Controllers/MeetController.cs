2703cbaf3004d802359247bbb891a285b8c54de2
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-17 06:52:04 -0700

    Meet Controller Created

---
+++ NETMeetApp/Controllers/MeetController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NETMeetApp.Controllers
+{
+    public class MeetController : Controller
+    {
+        [Route("meet/{meetId}")]
+        public IActionResult Index(string meetId)
+        {
+            ViewData["MeetingId"] = meetId;
+            return View();
+        }
+    }
+}
