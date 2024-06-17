df7499546990482d0fa5144454ef8f4bfc727162
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-17 05:19:51 -0700

    User Class Updated Added Age propery

--- NETMeetApp/Models/User.cs
+++ NETMeetApp/Models/User.cs
@@ -1,6 +1,13 @@
 ﻿namespace NETMeetApp.Models
 {
-    public class User
+    public abstract class User
     {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public enum Gender { Male, Female }
+        public bool IsTeacher { get; set; }
+        public bool IsAdmin { get; set; }
+
     }
 }
