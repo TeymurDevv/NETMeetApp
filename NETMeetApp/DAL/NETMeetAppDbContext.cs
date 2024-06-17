bf21809d68aaffed0c0847e6796c0f64f00ebd68
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-17 05:39:08 -0700

    Dbcontext Dbset Added

--- NETMeetApp/DAL/NETMeetAppDbContext.cs
+++ NETMeetApp/DAL/NETMeetAppDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NETMeetApp.Models;
+using NETMeetApp.Models.Common;
 
 namespace NETMeetApp.DAL
 {
     public class NETMeetAppDbContext : DbContext
     {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
         public NETMeetAppDbContext(DbContextOptions options) : base(options)
         {
 
