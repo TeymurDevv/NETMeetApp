fe07b16bc0751252b08284da5209ee688b970b5e
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-17 05:31:55 -0700

    Base Entity Updated

--- NETMeetApp/Models/Common/BaseEntity.cs
+++ NETMeetApp/Models/Common/BaseEntity.cs
@@ -3,5 +3,7 @@
     public class BaseEntity
     {
         public Guid Id { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime? UpdatedTime { get; set; }
     }
 }
