d33140d3cbc2aeb0504a66757facb3bfcbab1913
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-17 06:42:06 -0700

    SignalR MeetHub Created

---
+++ NETMeetApp/Hubs/MeetHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace NETMeetApp.Hubs
+{
+    public class MeetHub : Hub
+    {
+        public async Task JoinMeeting(string meetingId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, meetingId);
+        }
+
+        public async Task LeaveMeeting(string meetingId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, meetingId);
+        }
+
+        public async Task SendMessageToMeeting(string meetingId, string user, string message)
+        {
+            await Clients.Group(meetingId).SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
