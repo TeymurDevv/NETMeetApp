dd9cdc803072b141c508ca6db060820ddbb63467
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-17 05:30:08 -0700

    User Class Aded Email,Country,BioGraphy

--- NETMeetApp/Models/User.cs
+++ NETMeetApp/Models/User.cs
@@ -5,6 +5,9 @@
         public string Name { get; set; }
         public string SurName { get; set; }
         public int Age { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+        public string BioGraphy { get; set; }
         public enum Gender { Male, Female }
         public bool IsTeacher { get; set; }
         public bool IsAdmin { get; set; }
