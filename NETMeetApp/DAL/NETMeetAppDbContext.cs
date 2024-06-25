8303fe12d689a1ef77af2c75856f436b3195c015
Nihadname <nihadcoding@gmail.com>
2024-06-26 00:46:55 +0400

    done adding some a tags to admin panel

--- NETMeetApp/DAL/NETMeetAppDbContext.cs
+++ NETMeetApp/DAL/NETMeetAppDbContext.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using NETMeetApp.Models;
 
 namespace NETMeetApp.DAL
 {
-    public class NETMeetAppDbContext : IdentityDbContext
+    public class NETMeetAppDbContext : DbContext
     {
         public NETMeetAppDbContext(DbContextOptions options) : base(options)
         {
