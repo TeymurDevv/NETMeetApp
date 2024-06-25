40620114b7bc65dc2f06be1ed9918c2d36a8a049
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-25 15:59:55 -0700

    Register View Updated

--- NETMeetApp/ViewModels/Account/RegisterVm.cs
+++ NETMeetApp/ViewModels/Account/RegisterVm.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
 
 namespace NETMeetApp.ViewModels.Account
 {
