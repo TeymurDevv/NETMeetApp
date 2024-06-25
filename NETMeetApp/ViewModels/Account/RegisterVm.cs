8eefa6474e8a16def3b7c46d1bd9b32e05356bff
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-25 15:36:54 -0700

    RegistterVM Properites Added

--- NETMeetApp/ViewModels/Account/RegisterVm.cs
+++ NETMeetApp/ViewModels/Account/RegisterVm.cs
@@ -1,6 +1,20 @@
-﻿namespace NETMeetApp.ViewModels.Account
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NETMeetApp.ViewModels.Account
 {
     public class RegisterVM
     {
+        [Required,StringLength(100)]
+        public string FullName { get; set; }
+        [Required, StringLength(100)]
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required, DataType(DataType.Password), Compare(nameof(Password))]
+        public string RepeatPassword { get; set; }
+        [Required]
+        public bool IsTermsAccepted { get; set; }
+
     }
 }
