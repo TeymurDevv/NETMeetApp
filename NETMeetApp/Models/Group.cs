c56e3af66d295dbb91a448e07cc2b5f740acce56
Nihadname <nihadcoding@gmail.com>
2024-06-25 16:51:00 +0400

    some alterations in models and relationships

---
+++ NETMeetApp/Models/Group.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using NETMeetApp.Models.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NETMeetApp.Models
+{
+    public class Group:BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }    
+        public int Capacity { get; set; }
+        public string Image { get; set; }
+
+        public List<Student> Students { get; set; }
+        public Teacher Teacher { get; set; }
+        [ForeignKey("TeacherId")]
+
+        public int  TeacherId { get; set; }
+
+    }
+}
--- NETMeetApp/Models/Student.cs
+++ NETMeetApp/Models/Student.cs
@@ -1,7 +1,11 @@
 ﻿using NETMeetApp.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Student : User
 {
     public int Grade { get; set; }
+   public Group Group { get; set; }
+    [ForeignKey("GroupId")]
+    public int GroupId { get; set; }
 
 }
