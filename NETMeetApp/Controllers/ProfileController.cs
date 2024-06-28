5057340abcac794615b573a453e68353bb6ad052
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-28 17:57:13 +0300

    Profile Index finished for Logged in User

--- NETMeetApp/Controllers/ProfileController.cs
+++ NETMeetApp/Controllers/ProfileController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NETMeetApp.Models;
 
 namespace NETMeetApp.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public ProfileController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user is null) return RedirectToAction("Index", "Home");
+            return View(user);
+        }
+
+        public async Task<IActionResult> UserName(string? username)
+        {
+            if (username is null) return RedirectToAction("Index", "Home");
+            var user = await _userManager.FindByNameAsync(username);
+            if (user is null) return RedirectToAction("Index", "Home");
+            return View(user);
+
+
         }
     }
 }
