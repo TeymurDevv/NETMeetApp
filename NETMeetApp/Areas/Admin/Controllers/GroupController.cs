a0ea3a6dbbe352502b265cc23fb253a7ecb1a919
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-30 02:46:15 +0300

    Added Role Validation For GroupController

--- NETMeetApp/Areas/Admin/Controllers/GroupController.cs
+++ NETMeetApp/Areas/Admin/Controllers/GroupController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NETMeetApp.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "SuperAdmin,Admin")]
     [Area("Admin")]
     public class GroupController : Controller
     {
