f4c36e061e6fab173a81447a537e63d4e7a57976
TeymurDevv <transmissiyaservis@gmail.com>
2024-06-30 02:39:53 +0300

    AdminLayout Identity Information is done

--- NETMeetApp/Areas/Admin/Controllers/DashBoardController.cs
+++ NETMeetApp/Areas/Admin/Controllers/DashBoardController.cs
@@ -18,6 +18,8 @@ namespace NETMeetApp.Areas.Admin.Controllers
 
         public async Task<IActionResult> Index()
         {
+            var existUser = await _userManager.GetUserAsync(User);
+            ViewBag.User = existUser;
             ViewBag.StudentsCount = _userManager.Users.Where(s => s.UserType == Enums.UserType.Student).Count();
             ViewBag.TeachersCount = _userManager.Users.Where(s => s.UserType == Enums.UserType.Teacher).Count();
             ViewBag.AdminCount = _userManager.Users.Where(s => s.UserType == Enums.UserType.Admin).Count();
