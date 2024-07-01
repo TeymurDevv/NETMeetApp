67a5f93cbae2d0b9d6d0b115dea0ba99b0330f45
TeymurDevv <transmissiyaservis@gmail.com>
2024-07-01 18:39:14 +0300

    Hub Methods Updated to Handle The Dynamic Hub Join Events

--- NETMeetApp/Hubs/ChatHub.cs
+++ NETMeetApp/Hubs/ChatHub.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using NETMeetApp.Models;
 
 namespace NETMeetApp.Hubs
 {
     public class ChatHub : Hub
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ChatHub(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
         public override Task OnConnectedAsync()
         {
+            if (Context.User.Identity.IsAuthenticated)
+            {
+                var user = _userManager.FindByNameAsync(Context.User.Identity.Name).Result;
+                user.Connectionid = Context.ConnectionId;
+                var result = _userManager.UpdateAsync(user).Result;
+                Clients.All.SendAsync("OnConnect", user.Id);
+            }
+
             return base.OnConnectedAsync();
         }
         public override Task OnDisconnectedAsync(Exception? exception)
